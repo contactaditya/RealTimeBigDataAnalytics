5d5c8812208f966a762da41fba210e9d74576850
Aditya Gupta <contactadityag@gmail.com>
2018-03-03 07:13:26 -0500

    This is the MapReduce Job to combine the social indicators for each country

--- Final Project/Social Index/AggregateSocialDataJob.java
+++ Final Project/Social Index/AggregateSocialDataJob.java
@@ -18,24 +18,24 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-public class AggregateSocialDataJob {
-	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-		conf.set("mapreduce.output.textoutputformat.separator", ";");
+ public class AggregateSocialDataJob {
+    public static void main(String[] args) throws Exception {
+      Configuration conf = new Configuration();
+      conf.set("mapreduce.output.textoutputformat.separator", ";");
 		        
-  	   	Job job = new Job(conf);
-  	   	job.setJarByClass(AggregateSocialDataJob.class);
-        job.setJobName("AggregateSocialDataJob");
+      Job job = new Job(conf);
+      job.setJarByClass(AggregateSocialDataJob.class);
+      job.setJobName("AggregateSocialDataJob");
   	   	
-  	   	job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
+      job.setOutputKeyClass(Text.class);
+      job.setOutputValueClass(Text.class);
            
-        job.setMapperClass(AggregateSocialDataMapper.class);
-        job.setReducerClass(AggregateSocialDataReducer.class);
+      job.setMapperClass(AggregateSocialDataMapper.class);
+      job.setReducerClass(AggregateSocialDataReducer.class);
            
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+      FileInputFormat.addInputPath(job, new Path(args[0]));
+      FileOutputFormat.setOutputPath(job, new Path(args[1]));
            
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
-	}
-}
+      System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+ }
