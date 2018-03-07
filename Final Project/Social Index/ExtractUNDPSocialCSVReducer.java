61d849bb2d4310d2eba3dc660549ec42fe009532
Aditya Gupta <contactadityag@gmail.com>
2018-03-07 04:06:38 -0500

    This program is a Reducer to extract social data from UNDP.

--- Final Project/Social Index/ExtractUNDPSocialCSVReducer.java
+++ Final Project/Social Index/ExtractUNDPSocialCSVReducer.java
@@ -11,31 +11,28 @@ import java.io.IOException;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class ExtractUNDPSocialCSVReducer
-    extends Reducer<Text, Text, Text, Text> {
+public class ExtractUNDPSocialCSVReducer extends Reducer<Text, Text, Text, Text> {
   @Override
-  public void reduce(Text key, Iterable<Text> values, Context context) 
-      throws IOException, InterruptedException{
-	float total = 0;
-	float count = 0;
-	float min = 1000000;
-	float max = -1000000;
-	for (Text value : values) {
-	  float val = Float.parseFloat(value.toString());			  
-	  total += val;
-	  count++;
+  public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+    float total = 0;
+    float count = 0;
+    float min = 1000000;
+    float max = -1000000;
+    for (Text value : values) {
+      float val = Float.parseFloat(value.toString());			  
+      total += val;
+      count++;
 	  
-	  if (val > max) {
-		  max = val;
-	  }
-	  if (val < min) {
-		  min = val;
-	  }
-	}
-	float average = total / count;
-	String outputStr = String.valueOf(min) + " "
-	    + String.valueOf(average) + " " + String.valueOf(max);
+      if (val > max) {
+	max = val;
+      }
+      if (val < min) {
+	min = val;
+      }
+    }
+    float average = total / count;
+    String outputStr = String.valueOf(min) + " " + String.valueOf(average) + " " + String.valueOf(max);
 	
-	context.write(key, new Text(outputStr));
+    context.write(key, new Text(outputStr));
   }
 }
