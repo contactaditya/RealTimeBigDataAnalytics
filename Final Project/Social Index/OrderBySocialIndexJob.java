4945f6c23b77f12a72ac2726c9794be93343817b
Aditya Gupta <contactadityag@gmail.com>
2018-03-07 07:57:25 -0500

    This program is used to order countries by social index.

--- Final Project/Social Index/OrderBySocialIndexJob.java
+++ Final Project/Social Index/OrderBySocialIndexJob.java
@@ -15,27 +15,27 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class OrderBySocialIndexJob {
-	public static void main(String[] args) throws Exception {
-		if (args.length < 2) {
-		  System.err.println("Usage: OrderBySocialIndexJob <input path> <output path>");
-		}
+   public static void main(String[] args) throws Exception {
+      if (args.length < 2) {
+	System.err.println("Usage: OrderBySocialIndexJob <input path> <output path>");
+      }
 		
-		Configuration conf = new Configuration();
-		conf.set("mapreduce.output.textoutputformat.separator", ";");
+      Configuration conf = new Configuration();
+      conf.set("mapreduce.output.textoutputformat.separator", ";");
 
-		Job job = new Job(conf);
-		job.setJarByClass(OrderBySocialIndexJob.class);
-		job.setJobName("OrderBySocialIndexJob");
+      Job job = new Job(conf);
+      job.setJarByClass(OrderBySocialIndexJob.class);
+      job.setJobName("OrderBySocialIndexJob");
 		
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+      FileInputFormat.addInputPath(job, new Path(args[0]));
+      FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		
-		job.setMapperClass(OrderBySocialIndexMapper.class);
-		job.setReducerClass(OrderBySocialIndexReducer.class);
+      job.setMapperClass(OrderBySocialIndexMapper.class);
+      job.setReducerClass(OrderBySocialIndexReducer.class);
 		
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+      job.setOutputKeyClass(Text.class);
+      job.setOutputValueClass(Text.class);
 		
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
-	  }
+      System.exit(job.waitForCompletion(true) ? 0 : 1);
+   }
 }
