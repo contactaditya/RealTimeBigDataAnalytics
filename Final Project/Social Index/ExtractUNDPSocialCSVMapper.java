dfbf13c19798af55ee03fa78a999cbd59eca776b
Aditya Gupta <contactadityag@gmail.com>
2018-03-07 04:00:59 -0500

    This program is a Mapper to extract social data from UNDP.

--- Final Project/Social Index/ExtractUNDPSocialCSVMapper.java
+++ Final Project/Social Index/ExtractUNDPSocialCSVMapper.java
@@ -13,46 +13,43 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.conf.Configuration;
 
-public class ExtractUNDPSocialCSVMapper 
-    extends Mapper<LongWritable, Text, Text, Text>{
-
+public class ExtractUNDPSocialCSVMapper extends Mapper<LongWritable, Text, Text, Text>{
+	
   @Override
-  public void map(LongWritable key, Text value, Context context)
-      throws IOException, InterruptedException {
+  public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
     String line = value.toString();
-	if (!line.isEmpty()) {
+    if (!line.isEmpty()) {  
+      Configuration conf = context.getConfiguration();
+      String colsStr = conf.get("cols");
+      int cols = 0;
 	  
-	  Configuration conf = context.getConfiguration();
-	  String colsStr = conf.get("cols");
-	  int cols = 0;
-	  
-	  String[] tokens = line.split(",");
-	  if (colsStr.equals("all")) {
-		cols = tokens.length - 2;
-	  }
-	  else {
-		cols = Integer.parseInt(colsStr);  
-	  }
+      String[] tokens = line.split(",");
+      if (colsStr.equals("all")) {
+	cols = tokens.length - 2;
+      }
+      else {
+	cols = Integer.parseInt(colsStr);  
+      }
 		  
-	  String HDIRank = tokens[0]; 
-	  String country = tokens[1];
+      String HDIRank = tokens[0]; 
+      String country = tokens[1];
 		  
-	  if (country.charAt(0) == '\"') {
-		country += "," + tokens[2];
-		if (colsStr.equals("all")) {
-		  cols--;
-		}
-	  }
-		 
-	  for (int i = tokens.length - 1; i >= tokens.length - cols; i--) {
-		String nextVal = tokens[i];
-		if (nextVal.equals("")) {
-		  cols++;
-		}
-		else if (!nextVal.equals("..")) {
-		  context.write(new Text(country), new Text(nextVal));
-		}
-	  } 
+      if (country.charAt(0) == '\"') {
+        country += "," + tokens[2];
+	if (colsStr.equals("all")) {
+	  cols--;
 	}
+      }
+		 
+      for (int i = tokens.length - 1; i >= tokens.length - cols; i--) {
+	String nextVal = tokens[i];
+	if (nextVal.equals("")) {
+	  cols++;
+	}
+	else if (!nextVal.equals("..")) {
+	  context.write(new Text(country), new Text(nextVal));
+	}
+      } 
+    }
   }
 }
