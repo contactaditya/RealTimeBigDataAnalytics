2861b7ea5b398e233196accc1c76c640100418fc
Aditya Gupta <contactadityag@gmail.com>
2018-03-07 07:52:41 -0500

    This program is used to get the comparison values used to compute the weight assignments.

--- Final Project/Social Index/FindFilteredIndicatorMaxJob.java
+++ Final Project/Social Index/FindFilteredIndicatorMaxJob.java
@@ -15,27 +15,27 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class FindFilteredIndicatorMaxJob {
-	public static void main(String[] args) throws Exception {
-		if (args.length < 2) {
-		  System.err.println("Usage: FindFilteredIndicatorMaxJob <input path> <output path>");
-		}
+  public static void main(String[] args) throws Exception {
+     if (args.length < 2) {
+       System.err.println("Usage: FindFilteredIndicatorMaxJob <input path> <output path>");
+     }
 		
-		Configuration conf = new Configuration();
-		conf.set("mapreduce.output.textoutputformat.separator", ";");
+     Configuration conf = new Configuration();
+     conf.set("mapreduce.output.textoutputformat.separator", ";");
 		
-		Job job = new Job(conf);
-		job.setJarByClass(FindFilteredIndicatorMaxJob.class);
-		job.setJobName("FindFilteredIndicatorMaxJob");
+     Job job = new Job(conf);
+     job.setJarByClass(FindFilteredIndicatorMaxJob.class);
+     job.setJobName("FindFilteredIndicatorMaxJob");
 		
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+     FileInputFormat.addInputPath(job, new Path(args[0]));
+     FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		
-		job.setMapperClass(FindFilteredIndicatorMaxMapper.class);
-		job.setReducerClass(FindFilteredIndicatorMaxReducer.class);
+     job.setMapperClass(FindFilteredIndicatorMaxMapper.class);
+     job.setReducerClass(FindFilteredIndicatorMaxReducer.class);
 		
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+     job.setOutputKeyClass(Text.class);
+     job.setOutputValueClass(Text.class);
 		
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
-	  }
+     System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
 }
