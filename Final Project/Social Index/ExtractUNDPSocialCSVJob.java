fa90e590d5225e452eba4ce720f62a5256cfa14d
Aditya Gupta <contactadityag@gmail.com>
2018-03-06 22:20:52 -0500

    This is the program to extract UNDP Social data.

--- Final Project/Social Index/ExtractUNDPSocialCSVJob.java
+++ Final Project/Social Index/ExtractUNDPSocialCSVJob.java
@@ -15,33 +15,33 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class ExtractUNDPSocialCSVJob {
   public static void main(String[] args) throws Exception {
-	if (args.length < 2) {
-	  System.err.println("Usage: ExtractSocialJob <input path> <output path> [number of cols to read from the end]");
-	  System.exit(1);
-	}
+     if (args.length < 2) {
+       System.err.println("Usage: ExtractSocialJob <input path> <output path> [number of cols to read from the end]");
+       System.exit(1);
+     }
 	
-	org.apache.hadoop.conf.Configuration conf = new org.apache.hadoop.conf.Configuration();
+     org.apache.hadoop.conf.Configuration conf = new org.apache.hadoop.conf.Configuration();
 	
-	if (args.length > 2) {
-		conf.set("cols", args[2]);
-	}
-	else {
-		conf.set("cols", "all");
-	}
+     if (args.length > 2) {
+       conf.set("cols", args[2]);
+     }
+     else {
+       conf.set("cols", "all");
+     }
 	
-	Job job = new Job(conf);
-	job.setJarByClass(ExtractUNDPSocialCSVJob.class);
-	job.setJobName("ExtractSocialJob");
+     Job job = new Job(conf);
+     job.setJarByClass(ExtractUNDPSocialCSVJob.class);
+     job.setJobName("ExtractSocialJob");
 	
-	FileInputFormat.addInputPath(job,  new Path(args[0]));
-	FileOutputFormat.setOutputPath(job, new Path(args[1]));
+     FileInputFormat.addInputPath(job, new Path(args[0]));
+     FileOutputFormat.setOutputPath(job, new Path(args[1]));
 	
-	job.setMapperClass(ExtractUNDPSocialCSVMapper.class);
-	job.setReducerClass(ExtractUNDPSocialCSVReducer.class);
+     job.setMapperClass(ExtractUNDPSocialCSVMapper.class);
+     job.setReducerClass(ExtractUNDPSocialCSVReducer.class);
 	
-	job.setOutputKeyClass(Text.class);
-	job.setOutputValueClass(Text.class);
+     job.setOutputKeyClass(Text.class);
+     job.setOutputValueClass(Text.class);
 	
-	System.exit(job.waitForCompletion(true) ? 0 : 1);
+     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
