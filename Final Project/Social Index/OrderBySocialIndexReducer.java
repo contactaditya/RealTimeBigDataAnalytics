49028a200b5b537fdadd16f2d6221a93b75991bc
Aditya Gupta <contactadityag@gmail.com>
2018-03-07 07:44:22 -0500

    This program is used to order countries by social index.

--- Final Project/Social Index/OrderBySocialIndexReducer.java
+++ Final Project/Social Index/OrderBySocialIndexReducer.java
@@ -18,14 +18,12 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class OrderBySocialIndexReducer
-    extends Reducer<Text, Text, Text, Text> {
-	@Override
-	public void reduce(Text key, Iterable<Text> values, Context context)
-	    throws IOException, InterruptedException {
-	  for (Text value : values) {
-		double reinvertedSocialIndex = 1 / (Double.valueOf(key.toString()));
-		context.write(new Text(String.valueOf(reinvertedSocialIndex)), value);
-	  }
-	}
+public class OrderBySocialIndexReducer extends Reducer<Text, Text, Text, Text> {
+  @Override
+  public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+    for (Text value : values) {
+      double reinvertedSocialIndex = 1 / (Double.valueOf(key.toString()));
+      context.write(new Text(String.valueOf(reinvertedSocialIndex)), value);
+    }
+  }
 }
