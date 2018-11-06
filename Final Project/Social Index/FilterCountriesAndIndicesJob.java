477dfa716302884c16e460a0f8ad83b9e9c79518
Aditya Gupta <contactadityag@gmail.com>
2018-11-05 22:17:03 -0500

    This is a program to filter countries and indices.

--- Final Project/Social Index/FilterCountriesAndIndicesJob.java
+++ Final Project/Social Index/FilterCountriesAndIndicesJob.java
@@ -14,28 +14,28 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class FilterCountriesAndIndicesJob {
-	public static void main(String[] args) throws Exception {
-		if (args.length < 2) {
-		  System.err.println("Usage: FilterCountriesAndIndicesJob <input path> <output path>");
-		}
+  public class FilterCountriesAndIndicesJob {
+    public static void main(String[] args) throws Exception {
+      if (args.length < 2) {
+	System.err.println("Usage: FilterCountriesAndIndicesJob <input path> <output path>");
+      }
 		
-		Configuration conf = new Configuration();
-		conf.set("mapreduce.output.textoutputformat.separator", ";");
+      Configuration conf = new Configuration();
+      conf.set("mapreduce.output.textoutputformat.separator", ";");
 
-		Job job = new Job(conf);
-		job.setJarByClass(FilterCountriesAndIndicesJob.class);
-		job.setJobName("FilterCountriesAndIndicesJob");
+      Job job = new Job(conf);
+      job.setJarByClass(FilterCountriesAndIndicesJob.class);
+      job.setJobName("FilterCountriesAndIndicesJob");
 		
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+      FileInputFormat.addInputPath(job, new Path(args[0]));
+      FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		
-		job.setMapperClass(FilterCountriesAndIndicesMapper.class);
-		job.setReducerClass(FilterCountriesAndIndicesReducer.class);
+      job.setMapperClass(FilterCountriesAndIndicesMapper.class);
+      job.setReducerClass(FilterCountriesAndIndicesReducer.class);
 		
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+      job.setOutputKeyClass(Text.class);
+      job.setOutputValueClass(Text.class);
 		
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
-	  }
-}
+      System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+  }
