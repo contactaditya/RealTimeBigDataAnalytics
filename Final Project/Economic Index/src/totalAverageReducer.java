d6b998ca37a37642ec676c37efd10dfc0f63e262
Aditya Gupta <contactadityag@gmail.com>
2018-11-08 04:28:40 -0500

    This is a program for total Average Reducer.

--- Final Project/Economic Index/src/totalAverageReducer.java
+++ Final Project/Economic Index/src/totalAverageReducer.java
@@ -4,17 +4,19 @@ import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Reducer.Context;
+
 public class totalAverageReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
-	private Text toke = new Text();
-	private DoubleWritable val = new DoubleWritable();
-	private Double sum = (double) 0;
-	private int counter = 0;
-	public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException{
-		for(DoubleWritable val : values){
-			counter++;
-			sum += val.get()/1000;
-		}
-		val.set((sum/counter)*1000);
-		context.write(toke, val);
-	}
+  private Text toke = new Text();
+  private DoubleWritable val = new DoubleWritable();
+  private Double sum = (double) 0;
+  private int counter = 0; 
+	
+  public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+    for(DoubleWritable val : values) {
+      counter++;
+      sum += val.get() / 1000;
+    }
+    val.set((sum/counter) * 1000);
+    context.write(toke, val);
+  }
 }
