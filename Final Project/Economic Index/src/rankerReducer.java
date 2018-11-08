67f5a04cb7911c86d0926c5af2f35fdb2cfe82ea
Aditya Gupta <contactadityag@gmail.com>
2018-11-08 04:32:41 -0500

    This is a program for ranker Reducer.

--- Final Project/Economic Index/src/rankerReducer.java
+++ Final Project/Economic Index/src/rankerReducer.java
@@ -4,10 +4,11 @@ import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Reducer.Context;
+
 public class rankerReducer extends Reducer<DoubleWritable, Text, DoubleWritable, Text> {
-	public void reduce(DoubleWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
-		for(Text val : values){
-			context.write(key, val);
-		}
-	}
+  public void reduce(DoubleWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+    for(Text val : values) {
+      context.write(key, val);
+    }
+  }
 }
