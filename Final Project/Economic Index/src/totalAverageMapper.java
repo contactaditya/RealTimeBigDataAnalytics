ea8308698577c73e4d98fbe39a33b5195d548b90
Aditya Gupta <contactadityag@gmail.com>
2018-11-07 21:47:09 -0500

    This is a program for total average mapper.

--- Final Project/Economic Index/src/totalAverageMapper.java
+++ Final Project/Economic Index/src/totalAverageMapper.java
@@ -6,22 +6,23 @@ import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.io.DoubleWritable;
 import java.lang.Exception;
-public class totalAverageMapper extends Mapper<Object, Text, Text, DoubleWritable>{
-	private Text toke = new Text();
-	private DoubleWritable val = new DoubleWritable();
-	@Override
-	public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
-		String line = value.toString();
-		line = line.trim();
-		String[] tokens = line.split(";");
-		toke.set("");
-		if(tokens.length > 1){
-			try{
-				val.set(Double.parseDouble(tokens[1]));
-				context.write(toke, val);
-			}
-			catch(Exception ex){
-			}
-		}
-	}
+
+public class totalAverageMapper extends Mapper<Object, Text, Text, DoubleWritable> {
+  private Text toke = new Text();
+  private DoubleWritable val = new DoubleWritable();
+  @Override
+  public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+    String line = value.toString();
+    line = line.trim();
+    String[] tokens = line.split(";");
+    toke.set("");
+    if(tokens.length > 1) {
+      try {
+        val.set(Double.parseDouble(tokens[1]));
+	context.write(toke, val);
+      }
+      catch(Exception ex) {
+      }
+    }
+  }
 }
