7926a4a5e4259e9abd1e76618222f0d910aaccef
Aditya Gupta <contactadityag@gmail.com>
2018-11-08 04:31:10 -0500

    This is a program for ranker Mapper.

--- Final Project/Economic Index/src/rankerMapper.java
+++ Final Project/Economic Index/src/rankerMapper.java
@@ -6,16 +6,17 @@ import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.io.DoubleWritable;
 import java.lang.Exception;
-public class rankerMapper extends Mapper<Object, Text, DoubleWritable, Text>{
-	private Text toke = new Text();
-	private DoubleWritable val = new DoubleWritable();
-	@Override
-	public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
-		String line = value.toString();
-		line = line.trim();
-		String[] tokens = line.split(";");
-		toke.set(tokens[0]);
-		val.set(Double.parseDouble(tokens[1]));
-		context.write(val, toke);
-	}
+
+public class rankerMapper extends Mapper<Object, Text, DoubleWritable, Text> { 
+  private Text toke = new Text();
+  private DoubleWritable val = new DoubleWritable();
+  @Override
+  public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+    String line = value.toString();
+    line = line.trim();
+    String[] tokens = line.split(";");
+    toke.set(tokens[0]);
+    val.set(Double.parseDouble(tokens[1]));
+    context.write(val, toke);
+  }
 }
